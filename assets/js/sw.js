e4edad5caab4eb8a8d12564fd32259089576ed11
ZeddYu <zeddyu.lu@gmail.com>
2025-01-04 16:36:13 +0800

    del hash in css/js filename

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,60 +1,57 @@
-// importScripts("https://cdn.zdy.one/npm/workbox-cdn/workbox/workbox-sw.js");
-// importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js");
-importScripts("https://cdn.jsdelivr.net/npm/workbox-sw@7.3.0/build/workbox-sw.min.js");
+importScripts(
+    "https://cdn.jsdelivr.net/npm/workbox-sw@7.3.0/build/workbox-sw.min.js"
+);
 
 if (workbox) {
-	console.log(`Yay! Workbox is loaded 🎉`);
+    console.log(`Yay! Workbox is loaded 🎉`);
 
-	workbox.routing.registerRoute(
-		/\.(?:js|css)$/,
-		new workbox.strategies.StaleWhileRevalidate({
-			cacheName: "static-resources",
-		})
-	);
+    workbox.core.setCacheNameDetails({
+        prefix: "zeddyu_github_io",
+    });
 
-	// workbox.routing.registerRoute(
-	// 	"https://cdn.zdy.one/gh/ZeddYu/ZeddYu.github.io@master/ts/main.js",
-	// 	new workbox.strategies.StaleWhileRevalidate({
-	// 		cacheName: "static-resources",
-	// 	})
-	// );
+    workbox.precaching.precacheAndRoute([
+        { url: "/", revision: "1" },
+        { url: "/index.html", revision: "1" },
+		{ url: '/scss/style.min.css', revision: '1' },
+		{ url: '/ts/main.js', revision: '1' },
+    ]);
 
-	workbox.routing.registerRoute(
-		/\.(?:png|jpg|jpeg|svg|gif)$/,
-		new workbox.strategies.CacheFirst({
-			cacheName: "image-cache",
-			plugins: [
-				new workbox.expiration.ExpirationPlugin({
-					maxEntries: 20,
-					maxAgeSeconds: 7 * 24 * 60 * 60,
-				}),
-			],
-		})
-	);
+    workbox.routing.registerRoute(
+        /\.(?:js|css)$/,
+        new workbox.strategies.StaleWhileRevalidate({
+            cacheName: "static-resources",
+        })
+    );
 
-	// // googlefonts.zeddyu-lu.workers.dev
-	// workbox.routing.registerRoute(
-	// 	/^https:\/\/googlefonts\.zeddyu-lu\.workers\.dev/,
-	// 	new workbox.strategies.StaleWhileRevalidate({
-	// 		cacheName: "google-fonts-stylesheets",
-	// 	})
-	// );
+    workbox.routing.registerRoute(
+        /\.(?:png|jpg|jpeg|svg|gif)$/,
+        new workbox.strategies.CacheFirst({
+            cacheName: "image-cache",
+            plugins: [
+                new workbox.expiration.ExpirationPlugin({
+                    maxEntries: 20,
+                    maxAgeSeconds: 7 * 24 * 60 * 60,
+                }),
+            ],
+        })
+    );
 
-	workbox.routing.registerRoute(
-		/^https:\/\/googlefonts\.zeddyu-lu\.workers\.dev/,
-		new workbox.strategies.CacheFirst({
-			cacheName: "google-fonts-webfonts",
-			plugins: [
-				new workbox.cacheableResponse.CacheableResponsePlugin({
-					statuses: [0, 200],
-				}),
-				new workbox.expiration.ExpirationPlugin({
-					maxAgeSeconds: 60 * 60 * 24 * 365,
-					maxEntries: 30,
-				}),
-			],
-		})
-	);
+    workbox.routing.registerRoute(
+        new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"),
+        new workbox.strategies.CacheFirst({
+            cacheName: "google-fonts-webfonts",
+            plugins: [
+                new workbox.cacheableResponse.CacheableResponsePlugin({
+                    statuses: [0, 200],
+                }),
+                new workbox.expiration.ExpirationPlugin({
+                    maxAgeSeconds: 60 * 60 * 24 * 365,
+                    maxEntries: 30,
+                }),
+            ],
+        })
+    );
+
 } else {
-	console.log(`Boo! Workbox didn't load 😬`);
+    console.log(`Boo! Workbox didn't load 😬`);
 }
